8d14cd3397aa3945f771b9bffa0694ed3b2a9691
Francois Szczepkowski <francois.szcz@hotmail.fr>
2024-06-25 12:37:12 +0200

    ADD: Hexagone

---
+++ developpement/Fares/src/components/common/Card/Hexagon/Hexagon.tsx
@@ -0,0 +1,138 @@
+import React from "react";
+import "./Hexagon.scss";
+
+import FakeButton from "../../Button/FakeButton";
+
+interface HexagonProps {
+	backgroundClass?: string;
+	title: string;
+	text: string;
+	buttonLabel: string;
+	buttonHref: string;
+	buttonIcon?: React.ReactNode;
+	buttonClassName?: string;
+	white?: boolean; // Nouvelle prop pour gérer la classe blanc
+}
+
+const Hexagon: React.FC<HexagonProps> = ({
+	backgroundClass,
+	title,
+	text,
+	buttonLabel,
+	buttonHref,
+	buttonIcon,
+	buttonClassName,
+	white = false, // Valeur par défaut
+}) => {
+	return (
+		<div>
+			<div className={`hexagon ${white ? "blanc" : ""}`}>
+				<svg
+					className="svg"
+					xmlns="http://www.w3.org/2000/svg"
+					width="520"
+					height="574"
+					fill="none"
+					viewBox="0 0 520 574"
+				>
+					<g filter="url(#filter0_d_1_484)">
+						<path
+							fill="#E4032E"
+							d="M40 160.781c0-11.14 7.89-24.779 17.571-30.349L242.393 24.177c9.682-5.57 25.532-5.57 35.214 0l184.822 106.255c9.682 5.57 17.571 19.209 17.571 30.349v212.438c0 11.14-7.889 24.779-17.571 30.349L277.607 509.823c-9.682 5.569-25.532 5.569-35.214 0L57.571 403.639C47.89 398.069 40 384.43 40 373.291v-212.51z"
+						></path>
+					</g>
+					<defs>
+						<filter
+							id="filter0_d_1_484"
+							width="520"
+							height="574"
+							x="0"
+							y="0"
+							colorInterpolationFilters="sRGB"
+							filterUnits="userSpaceOnUse"
+						>
+							<feFlood floodOpacity="0" result="BackgroundImageFix"></feFlood>
+							<feColorMatrix
+								in="SourceAlpha"
+								result="hardAlpha"
+								values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+							></feColorMatrix>
+							<feOffset dy="20"></feOffset>
+							<feGaussianBlur stdDeviation="20"></feGaussianBlur>
+							<feComposite in2="hardAlpha" operator="out"></feComposite>
+							<feColorMatrix values="0 0 0 0 0.894118 0 0 0 0 0.0117647 0 0 0 0 0.180392 0 0 0 0.25 0"></feColorMatrix>
+							<feBlend
+								in2="BackgroundImageFix"
+								result="effect1_dropShadow_1_484"
+							></feBlend>
+							<feBlend
+								in="SourceGraphic"
+								in2="effect1_dropShadow_1_484"
+								result="shape"
+							></feBlend>
+						</filter>
+					</defs>
+				</svg>
+				<svg
+					className="svg svg--blanc"
+					xmlns="http://www.w3.org/2000/svg"
+					width="472"
+					height="526"
+					fill="none"
+					viewBox="0 0 472 526"
+				>
+					<g filter="url(#filter0_d_1_493)">
+						<path
+							fill="#fff"
+							d="M16 144.781c0-11.14 7.89-24.779 17.571-30.349L218.393 8.177c9.682-5.57 25.532-5.57 35.214 0l184.822 106.255c9.682 5.57 17.571 19.209 17.571 30.349v212.438c0 11.14-7.889 24.779-17.571 30.349L253.607 493.823c-9.682 5.569-25.532 5.569-35.214 0L33.571 387.639C23.89 382.069 16 368.43 16 357.291v-212.51z"
+						></path>
+					</g>
+					<defs>
+						<filter
+							id="filter0_d_1_493"
+							width="472"
+							height="526"
+							x="0"
+							y="0"
+							colorInterpolationFilters="sRGB"
+							filterUnits="userSpaceOnUse"
+						>
+							<feFlood floodOpacity="0" result="BackgroundImageFix"></feFlood>
+							<feColorMatrix
+								in="SourceAlpha"
+								result="hardAlpha"
+								values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+							></feColorMatrix>
+							<feOffset dy="12"></feOffset>
+							<feGaussianBlur stdDeviation="8"></feGaussianBlur>
+							<feComposite in2="hardAlpha" operator="out"></feComposite>
+							<feColorMatrix values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.06 0"></feColorMatrix>
+							<feBlend
+								in2="BackgroundImageFix"
+								result="effect1_dropShadow_1_493"
+							></feBlend>
+							<feBlend
+								in="SourceGraphic"
+								in2="effect1_dropShadow_1_493"
+								result="shape"
+							></feBlend>
+						</filter>
+					</defs>
+				</svg>
+				<div className={`bg-image ${backgroundClass}`}></div>
+				<div className="content">
+					<h3>{title}</h3>
+					<p>{text}</p>
+					<FakeButton
+						label={buttonLabel}
+						href={buttonHref}
+						icon={buttonIcon}
+						className={buttonClassName}
+					/>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Hexagon;
--- developpement/Fares/src/pages/Home/Home.tsx
+++ developpement/Fares/src/pages/Home/Home.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./Home.scss";
 
 import Header from "../../components/layout/Header/Header";
+import Hexagon from "../../components/common/Card/Hexagon/Hexagon";
 
 function Home(): React.ReactElement {
 	return (
@@ -9,6 +10,26 @@ function Home(): React.ReactElement {
 			<Header />
 			<section>
 				<h1>Welcome to Home Page</h1>
+				<div className="container">
+					<Hexagon
+						title="Tuberculose "
+						text="Le FARES est chargé d’améliorer la prise en charge et le suivi des patients tuberculeux et d’organiser le dépistage parmi les sujets à risque"
+						buttonLabel="En savoir plus"
+						buttonHref="#"
+						// buttonIcon={<FaArrowRight />}
+						buttonClassName="btn__fake--blanc"
+					/>
+					<Hexagon
+						white={true}
+						backgroundClass="bg-image--2"
+						title="Tabagisme "
+						text="L'équipe du Service Prévention Tabac met depuis 30 ans ses compétences au service des professionnels-relais des secteurs de l’éducation, de la santé et du social."
+						buttonLabel="En savoir plus"
+						buttonHref="#"
+						// buttonIcon={<FaArrowRight />}
+						buttonClassName="btn__fake--secondary"
+					/>
+				</div>
 			</section>
 		</>
 	);
